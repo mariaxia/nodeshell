124535feb72b26cd4b5bd2997f1f57f35b24184b
John Underwood <john.w.underwood.iii@gmail.com>
2017-01-16 14:09:44 -0500

    finished up to tail

--- bash.js
+++ bash.js
@@ -5,17 +5,10 @@ process.stdin.on('data', function(data){
   const input = data.toString().trim().split(' ');
   const cmd = input[0];
   const args = input.slice(1);
+  if (cmd in bash){
+    bash[cmd](args);
 
-  if (cmd === 'date'){
-    bash.date();
-  }
-  if (cmd === 'pwd'){
-    bash.pwd();
-  }
-  if (cmd === 'ls'){
-    bash.ls();
-  }
-  if (cmd === 'echo'){
-    bash.echo(args);
+  } else {
+    throw new Error('invalid command');
   }
 });
--- commands.js
+++ commands.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 
 module.exports = {
-  date: function(){
+  date: function(files){
     let date = new Date();
     process.stdout.write(date.toString());
     process.stdout.write('\nprompt > ');
   },
-  pwd: function(){
+  pwd: function(files){
     process.stdout.write(process.env.PWD);
     process.stdout.write('\nprompt > ');
   },
-  ls: function(){
+  ls: function(files){
     let directory = process.env.PWD;
     // fs.readdir(path[, options], callback(err, files))
 
@@ -26,12 +26,61 @@ module.exports = {
     // });
     setTimeout( function(){ process.stdout.write('prompt > ')}, 5 );
   },
-  echo: function(args){
+  echo: function(files){
     let string = '';
-    args.forEach(function(arg){
-      string += arg + ' ';
+    files.forEach(function(file){
+      string += file + ' ';
     });
     process.stdout.write(string.trim());
     process.stdout.write('\nprompt > ');
+  },
+
+  cat: function (files){
+    //get access to file using fs
+    //output the contents of the file
+    let counter = 0;
+    let contentsArr = [];
+
+    for (var i = 0; i < files.length; i++){
+      fs.readFile(files[i], function (err, contents) {
+        if (err) return console.error(err);
+        contentsArr[i] = contents;
+        counter++;
+        if (counter === files.length){
+          process.stdout.write(contentsArr.join('').toString());
+          process.stdout.write('\nprompt > ');
+        }
+      });
+    }
+  },
+
+  head: function (files){
+    fs.readFile(files[0], function(err, contents){
+      if (err) throw err;
+      let output = contents.toString().split('\n');
+      output = output.slice(0, 5);
+      process.stdout.write(output.join('\n').toString());
+      process.stdout.write('\nprompt > ');
+    });
+  },
+
+  tail: function (files){
+    fs.readFile(files[0], function(err, contents){
+      if (err) throw err;
+      let output = contents.toString().split('\n');
+      output = output.slice(-5);
+      process.stdout.write(output.join('\n').toString());
+      process.stdout.write('\nprompt > ');
+    });
+  },
+
+  sort: function (files){
+    fs.readFile(files[0], function(err, contents){
+      if (err) throw err;
+       let output = contents.toString().split('\n');
+      output = output.sort();
+      process.stdout.write(output.join('\n').toString());
+      process.stdout.write('\nprompt > ');
+    });
   }
 };
